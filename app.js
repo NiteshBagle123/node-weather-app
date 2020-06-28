03885024803b20f9393a8f9d8fa4f3bd749e7e15
Bagle <baglen@pun-baglen-m.ds.ad.adp.com>
2020-06-28 18:38:31 +0530

    callback implementation

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const request = require('request');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const url = 'http://api.weatherstack.com/current?access_key=9fa0798af554b948b4010a6010f0a271&query=37.8267,-122.4233';
+const place = process.argv[2] || 'India';
 
-request({ url }, (err, data)=> {
-    if(err){
-        console.log(`Error occured:${err}`);
-    } else {
-        const response = JSON.parse(data.body);
-        console.log(response.current);
+geocode(place, (error, { latitude, longitide, location } = {}) => {
+    if(error){
+        return console.log(error);
     }
+    console.log(latitude, longitide, location);
+    forecast(latitude, longitide, (error, { temperature, precip, description } = {}) => {
+        if(error){
+            console.log(error);
+        } else {
+            console.log(`current temperature is ${temperature} degrees out and ${precip}% chance of rain, weather description:${description}`);
+        }
+    })
 });
+
